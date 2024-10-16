29e893446ce81e384eeb730e25a87b5a54c8d51f
Mikko Tuomela <67043436+mikkotuomela@users.noreply.github.com>
2024-10-16 07:58:08 +0300

    Update partygenerator.js

--- src/partygenerator.js
+++ src/partygenerator.js
@@ -7,27 +7,27 @@ Probably doesn't work with IE because of template literals :------D
 function run() {
 
 	// Generate the party
-	const days = Math.floor(Math.random() * DAY_RANGE) + MIN_DAYS;	
-	const party = new Party(days);
+	const days         = Math.floor(Math.random() * DAY_RANGE) + MIN_DAYS;	
+	const party        = new Party(days);
 	party.generate();
 
 	// Generate output
-	const invParty = party.invitation.party;
-	const html_elements = ["competitions"];
+	const invParty     = party.invitation.party;
+	const htmlElements = ["competitions"];
 	const output = {
 		title:        `${party.name} ${party.year}`,
-		date:         `From ${party.formattedStartDate} to ${party.formattedEndDate}`,
+		date:         `From ${party.startDate.textg} to ${party.endDate.text}`,
 		venue:        `Party place: ${party.venue.name} (capacity: ${party.venue.capacity} computer places!!!) in ${party.city}, ${party.country}!`,
 		head:         `Head organizer: ${party.headOrg.nameWithFullHandle}`,
-		inv:          `Check out our invitation intro released on ${invParty.formattedStartDate} at ${invParty.name} ${invParty.year} in ${invParty.city}, ${invParty.country}!`,
+		inv:          `Check out our invitation intro released on ${invParty.startDate.text} at ${invParty.name} ${invParty.year} in ${invParty.city}, ${invParty.country}!`,
 		sponsor:      `Main sponsor: ${party.mainSponsor.fullName}`,
 		competitions: `We have the following competitions:<br />${party.getCompetitionsText()}`,
 		generator:    "",
 	}
 	
-	// Insert output
+	// Insert output using jQuery
 	Object.keys(output).forEach(key => {
-		html_elements.includes(key)
+		htmlElements.includes(key)
 			? $("#" + key).html(output[key])
 			: $("#" + key).text(output[key]);
 	})
