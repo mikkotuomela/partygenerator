131fe362c2f60a162e36faeadd14c2bb687252fe
Mikko Tuomela <67043436+mikkotuomela@users.noreply.github.com>
2024-10-16 09:02:56 +0300

    Update production.js

--- src/production.js
+++ src/production.js
@@ -2,5 +2,20 @@
 class Production {
 	constructor(days) {
 		this.party = new Party(days);
+    this.name = this._getRandomName();
+    console.log(`Production created: ${this.name}`);
 	}
+  // Get random production name
+  _getRandomName() {
+    return 'randomName';
+  }
+}
+
+class Invitation extends Production {
+  constructor() {
+    super();
+  }
+  _getRandomName() {
+    return 'introName';
+  }
 }
