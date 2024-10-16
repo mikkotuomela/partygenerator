24ce19e1d8611a096939d87dbca7266004c82d63
Mikko Tuomela <67043436+mikkotuomela@users.noreply.github.com>
2024-10-16 08:30:15 +0300

    Update venue.js

--- src/venue.js
+++ src/venue.js
@@ -1,14 +1,13 @@
 // Venue class
 class Venue {
 	constructor() {
-		this.name = this.getName();
+		this.name     = this.getRandomName();
 		this.capacity = Math.floor(2 ** (Math.random() * 11)) * 10;
-		//this.capacity = rand(1, 200) * 50;
 		console.log(`Venue created: ${this.name}`);
 	}
 	// Get a random venue name
-	getName() {
+	getRandomName() {
 		const company = new Company();
-		return `${company.name} ${VENUE_END.sample()}`;
+		return `${company.name} ${words.venue.end.sample()}`;
 	}
 }
