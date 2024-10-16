ed1242e33e8d54d097eb0d56f50b5b266e10b44e
Mikko Tuomela <67043436+mikkotuomela@users.noreply.github.com>
2024-10-16 09:51:28 +0300

    Update program.js

--- src/program.js
+++ src/program.js
@@ -1,22 +1,6 @@
 // Program class
 class Program {
   constructor() {
-    this.competitions     = _getRandomCompetitions();
-	  this.competitionsHtml = _getCompetitionsHtml();
+    this.competitions = new Competitions();
   }
-  // Generate competitions
-	_getRandomCompetitions() {
-		let competitions = [];
-		for (let competition_i = 0; competition_i < config.competitions; competition_i++)
-			competitions.push(new Competition());
-		return competitions;
-	}
-	// Get text version of competition list
-	_getCompetitionsHtml() {
-		let competitionsText = '';
-		this.competitions.forEach(competition => {
-			competitionsText += `${competition.name}<br />`;
-		});
-		return competitionsText;
-	}
 }
