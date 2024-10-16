6d4a623afd4619c3d8826c0248e243f63aedc6ed
Mikko Tuomela <67043436+mikkotuomela@users.noreply.github.com>
2024-10-16 07:31:55 +0300

    Update partygenerator.js

--- src/partygenerator.js
+++ src/partygenerator.js
@@ -15,14 +15,14 @@ function run() {
 	const invParty = party.invitation.party;
 	const html_elements = ["competitions"];
 	const output = {
-		title: `${party.name} ${party.year}`,
-		date: `From ${party.formattedStartDate} to ${party.formattedEndDate}`,
-		venue: `Party place: ${party.venue.name} (capacity: ${party.venue.capacity} computer places!!!) in ${party.city}, ${party.country}!`,
-		head: `Head organizer: ${party.headOrg.nameWithFullHandle}`,
-		inv: `Check out our invitation intro released on ${invParty.formattedStartDate} at ${invParty.name} ${invParty.year} in ${invParty.city}, ${invParty.country}!`,
-		sponsor: `Main sponsor: ${party.mainSponsor.fullName}`,
+		title:        `${party.name} ${party.year}`,
+		date:         `From ${party.formattedStartDate} to ${party.formattedEndDate}`,
+		venue:        `Party place: ${party.venue.name} (capacity: ${party.venue.capacity} computer places!!!) in ${party.city}, ${party.country}!`,
+		head:         `Head organizer: ${party.headOrg.nameWithFullHandle}`,
+		inv:          `Check out our invitation intro released on ${invParty.formattedStartDate} at ${invParty.name} ${invParty.year} in ${invParty.city}, ${invParty.country}!`,
+		sponsor:      `Main sponsor: ${party.mainSponsor.fullName}`,
 		competitions: `We have the following competitions:<br />${party.getCompetitionsText()}`,
-		generator: "",
+		generator:    "",
 	}
 	
 	// Insert output
