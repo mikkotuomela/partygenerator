f6234038d004a4b2d11db35993c689d9d197f567
Mikko Tuomela <67043436+mikkotuomela@users.noreply.github.com>
2024-10-16 08:13:11 +0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,60 +1,53 @@
-// Text generation constants
-const PARTY_START       = new List("New Finnish Best Oldschool Retro Swedish Japanese Special Illegal Small Random Generic Stolen Last Final First Drunk Obscure Christian Ultimate The The The Another That Weird Alternative");
-const PARTY_MIDDLE      = new List("Summer Winter Alternative Sauna C64 Amiga Atari Apple Dark Demo Booze Vodka Coding GFX CGI AI Machine Perl Hacker Data Cracker Stork Group Intro MSX Färjan Bar Restaurant Train Mod Hardware Horror LAN");
-const PARTY_END         = new List("Party Event Meeting Copyparty Gathering Party Party Megaparty Artparty Evening Weekend Assembly Afterparty Jamboree Conference Convention Unconference Workshop Jam Cruise");
-const COMPANY_START     = new List("Fish Tech Graph Angry Micro Mega Best Computer Game Video");
-const COMPANY_END       = new List("Birds Wizards Hub Tube App Team");
-const COMPANY_TYPE      = new List("Ltd Oy Oyj Plc Corp Corporation");
-const VENUE_END         = new List("Hall Colosseum Stadium Arena Center eSportsCenter eSportsArena Theatre Cinema Sauna Room Basement Restaurant Club");
-const FIRST_NAME        = new List("Mikko Kristoffer Felice Anni Kim Matti Chris Nick Pekka Al Jeff");
-const LAST_NAME         = new List("Tuomela Lawson Murtosaari Viljanen Koskimies Spowart Lowe Minter Virtanen Smith Hubbard Laakkonen Aakko");
-const HANDLE_START      = new List("Fake Big Bad Cute Dark Murder Laser Darth Dr Mr Sir Mrs Miss Professor Real True Mini Max Pissed Wiz Lol Bass Magic Rave Cereal Techno");
-const HANDLE_END        = new List("Wolf Nut Pharaoh Jesus Buddha Nuke Space Planet God Cat Kitty Bat Cadet Coder Wizard Ninja Girl Boy Guy Vader Killer Bob Wizard Witch Hacker Conan Arnold Punk Pope Geisha Shogun Emperor King Queen Prince Princess Duke Nuke Joker");
-const GROUP_START       = new List("Wild Dark Elite Amiga Gold Cool Complex Byte Anarchy Virtual Psycho Mad");
-const GROUP_END         = new List("Coders Group Team Pirates Dreams Crew Men Guys Boys");
-const COMPETITION_START = new List("Automated Robotic Impossible Radical Political Unofficial Uncensored Reverse Alternative Dynamic Unlimited 1-on-1 Oldschool Realtime Tiny Live Extreme Inverted Beginner Real Obscure Executable Themed Freestyle Textmode Funniest Ridiculous Silent Quiet Slow Fast Simulated Emulated Kinky Classic Random Combined 1970s 1980s 1990s");
-const COMPETITION_END   = new List("Wild Vallejo Short Färjan Calculator Supercomputer BASIC MSX Analogue Invitation 1-bit Perl Java Flash Steampunk MIDI ProTracker AGA VGA 3DO PlayStation Generated Fractal Art Crack Hardware Nintendo Data Computer Software Plasma Phong Gouraud Bootsector Crayon Game 3D 2D 4k 256B Roguelike Amiga C64 Fake Spectrum AI Party");
-const COMPETITION_TYPE  = new List("Demo Demo Demo Demo Intro Intro Dentro Graphics Graphics Knitting Poetry Video Gaming Gaming Hacking Cosplay Music Music Sound Zoomer Effect Throwing Boozing Visuals VJ DJ Dancing");
-const COMPETITION_WORD  = new List("Competition Performance Competition Competition Contest Showdown");
-
 // Words for name generation
-const words = {
+const wordLists = {
   party: {
-    start:  "New Finnish Best Oldschool Retro Swedish Japanese Special Illegal Small Random Generic Stolen Last Final First Drunk Obscure Christian Ultimate The The The Another That Weird Alternative",
-    middle: "Summer Winter Alternative Sauna C64 Amiga Atari Apple Dark Demo Booze Vodka Coding GFX CGI AI Machine Perl Hacker Data Cracker Stork Group Intro MSX Färjan Bar Restaurant Train Mod Hardware Horror LAN",
-    end:    "Party Event Meeting Copyparty Gathering Party Party Megaparty Artparty Evening Weekend Assembly Afterparty Jamboree Conference Convention Unconference Workshop Jam Cruise",
+    start:  'New Finnish Best Oldschool Retro Swedish Japanese Special Illegal Small Random Generic Stolen Last Final First Drunk Obscure Christian Ultimate The The The Another That Weird Alternative',
+    middle: 'Summer Winter Alternative Sauna C64 Amiga Atari Apple Dark Demo Booze Vodka Coding GFX CGI AI Machine Perl Hacker Data Cracker Stork Group Intro MSX Färjan Bar Restaurant Train Mod Hardware Horror LAN',
+    end:    'Party Event Meeting Copyparty Gathering Party Party Megaparty Artparty Evening Weekend Assembly Afterparty Jamboree Conference Convention Unconference Workshop Jam Cruise',
   },
   company: {
-    start:  "Fish Tech Graph Angry Micro Mega Best Computer Game Video",
-    end:    "Birds Wizards Hub Tube App Team",
-    type:   "Ltd Oy Oyj Plc Corp Corporation",
+    start:  'Fish Tech Graph Angry Micro Mega Best Computer Game Video',
+    end:    'Birds Wizards Hub Tube App Team',
+    type:   'Ltd Oy Oyj Plc Corp Corporation',
   },
   venue: {
-    end:    "Hall Colosseum Stadium Arena Center eSportsCenter eSportsArena Theatre Cinema Sauna Room Basement Restaurant Club",
+    end:    'Hall Colosseum Stadium Arena Center eSportsCenter eSportsArena Theatre Cinema Sauna Room Basement Restaurant Club',
   },
   name: {
-    first:  "Mikko Kristoffer Felice Anni Kim Matti Chris Nick Pekka Al Jeff",
-    last:   "Tuomela Lawson Murtosaari Viljanen Koskimies Spowart Lowe Minter Virtanen Smith Hubbard Laakkonen Aakko",
+    first:  'Mikko Kristoffer Felice Anni Kim Matti Chris Nick Pekka Al Jeff',
+    last:   'Tuomela Lawson Murtosaari Viljanen Koskimies Spowart Lowe Minter Virtanen Smith Hubbard Laakkonen Aakko',
   },
   handle: {
-    start:  "Fake Big Bad Cute Dark Murder Laser Darth Dr Mr Sir Mrs Miss Professor Real True Mini Max Pissed Wiz Lol Bass Magic Rave Cereal Techno",
-    end:    "Wolf Nut Pharaoh Jesus Buddha Nuke Space Planet God Cat Kitty Bat Cadet Coder Wizard Ninja Girl Boy Guy Vader Killer Bob Wizard Witch Hacker Conan Arnold Punk Pope Geisha Shogun Emperor King Queen Prince Princess Duke Nuke Joker",
+    start:  'Fake Big Bad Cute Dark Murder Laser Darth Dr Mr Sir Mrs Miss Professor Real True Mini Max Pissed Wiz Lol Bass Magic Rave Cereal Techno',
+    end:    'Wolf Nut Pharaoh Jesus Buddha Nuke Space Planet God Cat Kitty Bat Cadet Coder Wizard Ninja Girl Boy Guy Vader Killer Bob Wizard Witch Hacker Conan Arnold Punk Pope Geisha Shogun Emperor King Queen Prince Princess Duke Nuke Joker',
   },
   group: {
-    start: "Wild Dark Elite Amiga Gold Cool Complex Byte Anarchy Virtual Psycho Mad",
-    end:   "Coders Group Team Pirates Dreams Crew Men Guys Boys",
+    start: 'Wild Dark Elite Amiga Gold Cool Complex Byte Anarchy Virtual Psycho Mad',
+    end:   'Coders Group Team Pirates Dreams Crew Men Guys Boys',
   },
   competition: {
-    start: "Automated Robotic Impossible Radical Political Unofficial Uncensored Reverse Alternative Dynamic Unlimited 1-on-1 Oldschool Realtime Tiny Live Extreme Inverted Beginner Real Obscure Executable Themed Freestyle Textmode Funniest Ridiculous Silent Quiet Slow Fast Simulated Emulated Kinky Classic Random Combined 1970s 1980s 1990s",
-    end:   "Wild Vallejo Short Färjan Calculator Supercomputer BASIC MSX Analogue Invitation 1-bit Perl Java Flash Steampunk MIDI ProTracker AGA VGA 3DO PlayStation Generated Fractal Art Crack Hardware Nintendo Data Computer Software Plasma Phong Gouraud Mainframe Bootsector Crayon Game 3D 2D 4k 256B Roguelike Amiga C64 Fake Spectrum AI Party",
-    type:  "Demo Demo Demo Demo Intro Intro Dentro Graphics Graphics Knitting Poetry Video Gaming Gaming Hacking Cosplay Music Music Sound Zoomer Effect Throwing Boozing Visuals VJ DJ Dancing",
-    word:  "Competition Performance Competition Competition Contest Showdown",
+    start: 'Automated Robotic Impossible Radical Political Unofficial Uncensored Reverse Alternative Dynamic Unlimited 1-on-1 Oldschool Realtime Tiny Live Extreme Inverted Beginner Real Obscure Executable Themed Freestyle Textmode Funniest Ridiculous Silent Quiet Slow Fast Simulated Emulated Kinky Classic Random Combined 1970s 1980s 1990s',
+    end:   'Wild Vallejo Short Färjan Calculator Supercomputer BASIC MSX Analogue Invitation 1-bit Perl Java Flash Steampunk MIDI ProTracker AGA VGA 3DO PlayStation Generated Fractal Art Crack Hardware Nintendo Data Computer Software Plasma Phong Gouraud Mainframe Bootsector Crayon Game 3D 2D 4k 256B Roguelike Amiga C64 Fake Spectrum AI Party',
+    type:  'Demo Demo Demo Demo Intro Intro Dentro Graphics Graphics Knitting Poetry Video Gaming Gaming Hacking Cosplay Music Music Sound Zoomer Effect Throwing Boozing Visuals VJ DJ Dancing',
+    word:  'Competition Performance Competition Competition Contest Showdown',
   }
 }
 
+// Convert everything into List objects
+var words = {};
+wordLists.forEach(theme => {
+  words[theme] = {};
+  wordLists[theme].each(key => {
+    words[theme][key] = new WordList(wordLists[theme][key]);
+  });
+});
+
 // Other constants
-const MIN_DAYS          = 180;
-const DAY_RANGE         = 300;
-const LOCALE            = "en-US";
-const COMPETITIONS      = 10;
-const FAKER_AVAILABLE   = testFaker();
+const config = {
+  minDays:        180,
+  dayRange:       300,
+  locale:         'en-US',
+  competitions:   10,
+  fakerAvailable: testFaker(),
+}
+
