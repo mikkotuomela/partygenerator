0d37d734d152e5732fc11dbd68ff3ceb4ab6da09
Mikko Tuomela <67043436+mikkotuomela@users.noreply.github.com>
2024-10-16 08:28:30 +0300

    Update person.js

--- src/person.js
+++ src/person.js
@@ -1,9 +1,9 @@
 // Person class
 class Person {
 	constructor() {
-		this.firstName = FIRST_NAME.sample();
-		this.lastName = LAST_NAME.sample();
-		this.name = `${this.firstName} ${this.lastName}`;
+		this.firstName = words.name.first.sample();
+		this.lastName  = words.name.last.sample();
+		this.name      = `${this.firstName} ${this.lastName}`;
 		console.log(`Person created: ${this.name}`);
 	}
 }
@@ -13,14 +13,14 @@ class Scener extends Person {
 	constructor() {
 		super();
 		this.handle = this.getHandle();
-		this.group = new Group();
-		this.nameWithHandle = `${this.firstName} "${this.handle}" ${this.lastName}`;
+		this.group  = new Group();
+		this.nameWithHandle     = `${this.firstName} "${this.handle}" ${this.lastName}`;
 		this.nameWithFullHandle = `${this.firstName} "${this.handle}/${this.group.name}" ${this.lastName}`;
 	}
 	// Get scene handle
 	getHandle() {
-		const handleStart = HANDLE_START.sample();
-		const handleEnd = HANDLE_END.sample();
-		return handleStart + handleEnd;
+		const handleStart = words.handle.start.sample();
+		const handleEnd   = words.handle.end.sample();
+		return `${handleStart}${handleEnd}`;
 	}
 }
