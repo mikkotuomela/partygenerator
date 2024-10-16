018c46e7d6ef1772fb753e669bf0c362dbccbdfa
Mikko Tuomela <67043436+mikkotuomela@users.noreply.github.com>
2024-10-16 08:39:50 +0300

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,22 +1,26 @@
 // List class
 class WordList {
 	constructor(list) {
-		this.list = list;
-		this.items = list.split(" ");
+		this.list  = list;
+		this.items = list.split(' ');
 	}
 	sample() {
 		return this.items[rand(this.items.length, 0)];
 	}
 }
 
+Array.prototype.random = function () {
+  return this[rand(this.length, 0)];
+}
+
 // Test if faker is available
 function testFaker() {
 	try {
 		const test = faker.address.city();
-		console.log("Faker is available");
+		console.log('Faker is available');
 		return true;
 	} catch (e) {
-		console.log("Faker is not available");
+		console.log('Faker is not available');
 		return false;
 	}
 }
@@ -28,6 +32,6 @@ function rand(min, max) {
 
 // Helper function to sample items from a string-based list
 function sample(items) {
-	items = items.split(" ");
+	items = items.split(' ');
 	return items[rand(items.length, 0)];
 }
