f9431df25ea5e88cf003a2b125178baafa6fa778
Mikko Tuomela <67043436+mikkotuomela@users.noreply.github.com>
2024-10-16 08:32:47 +0300

    Update competition.js

--- src/competition.js
+++ src/competition.js
@@ -2,23 +2,23 @@
 class Competition {
 	constructor() {
 		this.sponsor = new Company();
-		this.name = this.getName();
+		this.name    = this.getRandomName();
 		console.log(`Competition created: ${this.name}`);
 	}
 	// Generate a competition name
-	getName() {
-		const competitionStart = new List(words.competition.start).sample();
-		const competitionStart = COMPETITION_START.sample();
-		const competitionEnd = COMPETITION_END.sample();
-		const competitionType = COMPETITION_TYPE.sample();
-		const competitionWord = COMPETITION_WORD.sample();
-		let competitionName = `${competitionStart} ${competitionEnd} ${competitionType} ${competitionWord}`;
-		if (rand(10, 1) == 1) {
+	getRandomName() {
+		const competitionStart = words.competition.start.sample();
+		const competitionEnd   = words.competition.end.sample();
+		const competitionType  = words.competition.type.sample();
+		const competitionWord  = words.competition.word.sample();
+		let competitionName    = `${competitionStart} ${competitionEnd} ${competitionType} ${competitionWord}`;
+
+    // Random modifications
+		if (rand(10, 1) == 1)
 			competitionName = `Unofficial ${competitionName}`;
-		}
-		if (rand(10, 1) == 1) {
+		if (rand(10, 1) == 1)
 			competitionName = `${competitionName} sponsored by ${this.sponsor.fullName}`;
-		}
+    
 		return competitionName;
 	}
 }
