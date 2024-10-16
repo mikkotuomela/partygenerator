0131f037ef185c31aad559612b157ecdfe166f93
Mikko Tuomela <67043436+mikkotuomela@users.noreply.github.com>
2024-10-16 08:43:02 +0300

    Update date.js

--- src/date.js
+++ src/date.js
@@ -2,14 +2,14 @@
 class PartyDate extends Date {
   constructor(days = 0) {
     super();
-		this.setDate(this.getDate() + days);
+    this.setDate(this.getDate() + days);
     this.text = this.getFormattedText();
     this.year = this.getFullYear();
   }
   // Get nicely formatted date
   getFormattedText()  {
 		const options = { year: 'numeric', month: 'long', day: 'numeric' };
-		return this.toLocaleDateString(LOCALE, options);
+		return this.toLocaleDateString(config.locale, options);
   }
 }
 
