0e531a86ad4180e1902b221b7e70f598727f9ff1
Mikko Tuomela <67043436+mikkotuomela@users.noreply.github.com>
2024-10-17 09:00:08 +0300

    Update universe.js

--- src/universe.js
+++ src/universe.js
@@ -1,18 +1,21 @@
 // Universe class
 class Universe {
   constructor() {
-    this.sceners = this._getRandomSceners();
+    this.sceners   = this._getRandomitems(Scener, config.universe.population.sceners);
+    this.companies = this._getRandomItems(Company, config.universe.population.companies);
   }
   // Get one scener
   getScener() {
     return this.sceners.pop();
   }
-  // Create sceners
-  _getRandomSceners() {
-    sceners = []
-    for (let scener_i = 0; scener_i < config.universe.population.sceners; scener_i++) {
-      sceners.push(new Scener());
+  getCompany() {
+    return this.companies.pop();
+  }
+  _getRandomItems(ItemType, population) {
+    let items = [];
+    for (let item_i = 0; item_i < population, item_i++) {
+      items.push(new ItemType());
     }
-    return sceners;
+    return items;
   }
 }
