5b812f0da1bf879359fcee884bcb71d5c8642687
Mikko Tuomela <67043436+mikkotuomela@users.noreply.github.com>
2024-10-17 13:43:28 +0300

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -4,8 +4,17 @@ function rand(min, max) {
 }
 
 // Add random() method to array prototype
-Array.prototype.random = function () {
-  return this[rand(this.length, 0)];
+// If n == 1, return a random element
+// If n != 1, return n elements as an array
+Array.prototype.random = function (n = 1) {
+  if (n == 1) return this[rand(this.length, 0)];
+  if (n > this.length) n = this.length;
+  const shuffled = this.slice();
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled.slice(0, n);
 }
 
 // Test if faker is available
